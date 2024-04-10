9b8d0fa4436fea91a27d852acf88e90cac609340
Kimhyeok <kimmang940715@naver.com>
2024-04-11 02:15:08 +0900

    feat : 2진수, 10진수

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -1,7 +1,18 @@
 ﻿#include <iostream>
 using namespace std;
 
+// char : 1byte
+// short : 2byte
+// int : 4byte
+// __int64(long long) : 8byte
+
+int hp;
+short mp;
+
 int main()
 {
+	hp = 0xF;
+	mp = 15;
 
+	cout << mp << hp; 
 }
