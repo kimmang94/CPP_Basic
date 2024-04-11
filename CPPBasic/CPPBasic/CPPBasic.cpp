5f9df85e3904509c4726eb9f6de3c8b43f72eba9
Kimhyeok <kimmang940715@naver.com>
2024-04-11 11:39:22 +0900

    feat : if문과 swithc 분기문

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -1,15 +1,21 @@
 ﻿#include <iostream>
 using namespace std;
 
-int a;
+int hp;
 int b;
 
 int main()
 {
-	// ==
-	// !=
-	// <
-	// <=
-	// >
-	// >=
+	hp = 100;
+
+	hp -= 200;
+
+	if (hp > 0)
+	{
+		cout << "생존";
+	}
+	else
+	{
+		cout << "사망";
+	}
 }
