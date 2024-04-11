372b09dbe66a74fde7c3db134e3d1d6daab0c6ad
Kimhyeok <kimmang940715@naver.com>
2024-04-11 12:05:32 +0900

    feat : 별찍기 문제 해결

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -1,9 +1,23 @@
 ﻿#include <iostream>
 using namespace std;
 
+int cnt;
 int main()
 {
+	cin >> cnt;
 	// 별찍기
+#pragma region 별찍기
+	for (int i = 0; i <= cnt; i++)
+	{
+		for (int j = 0; j <= i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+#pragma endregion
+
+	
 	// 구구단
 	// 간단한 전투
 	// 가위 바위 보
