49b8cbcb24b3388e6aeec07d77ed6757e75d8e1f
Kimhyeok <kimmang940715@naver.com>
2024-04-11 12:03:04 +0900

    feat : 연습문제

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -1,20 +1,10 @@
 ﻿#include <iostream>
 using namespace std;
-// [변수타입] [변수명] ;
-// 변수명은 이름을 이상하게 짓지말고 어떤용도로 쓰일지 적는다. 대소문자 구분
-// Ctrl + Shift + b 빌드하기 단축키
-
-// char : 1byte
-// short : 2byte
-// int : 4byte
-// __int64 (long long) : 8byte
-int hp;
-int maxHp;
 
 int main()
 {
-    maxHp = 200;
-    hp = maxHp;
-    cout << hp;
-    //std::cout << "Hello World!\n";
+	// 별찍기
+	// 구구단
+	// 간단한 전투
+	// 가위 바위 보
 }
