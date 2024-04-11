15b5c31597bd1af81e68b24c868707ef90159c34
Kimhyeok <kimmang940715@naver.com>
2024-04-11 12:27:19 +0900

    feat : 구구단 문제 해결

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -7,18 +7,27 @@ int main()
 	cin >> cnt;
 	// 별찍기
 #pragma region 별찍기
-	for (int i = 0; i <= cnt; i++)
+	//for (int i = 0; i <= cnt; i++)
+	//{
+	//	for (int j = 0; j <= i; j++)
+	//	{
+	//		cout << "*";
+	//	}
+	//	cout << endl;
+	//}
+#pragma endregion
+
+	// 구구단
+#pragma region 구구단
+	for (int num = 2; num <= 9; num++)
 	{
-		for (int j = 0; j <= i; j++)
+		for (int j = 1; j <= 9; j++)
 		{
-			cout << "*";
+			cout << num << "*" << j << "=" << num * j << endl;
 		}
-		cout << endl;
 	}
 #pragma endregion
 
-	
-	// 구구단
 	// 간단한 전투
 	// 가위 바위 보
 }
