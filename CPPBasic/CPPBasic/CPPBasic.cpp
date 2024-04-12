7f78d1415f74df2ce095f45f0a1d436280092d6c
Kimhyeok <kimmang940715@naver.com>
2024-04-12 12:31:07 +0900

    feat : 기존 내용 주석 처리

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -30,35 +30,106 @@ int main()
 
 	// 간단한 전투
 #pragma region 간단한 전투
-	int round = 1;
-	int hp = 100;
-	int damage = 10;
-	
-	while (true)
-	{
-		// 전투 진행
-		hp -= damage;
-		if (hp <= 0)
-		{
-			hp = 0;
-			cout << "Round : " << round << endl <<"몬스터 체력 : " << hp << endl;
-			
-		}
+	//int round = 1;
+	//int hp = 100;
+	//int damage = 10;
+	//
+	//while (true)
+	//{
+	//	// 전투 진행
+	//	hp -= damage;
+	//	if (hp <= 0)
+	//	{
+	//		hp = 0;
+	//		cout << "Round : " << round << endl <<"몬스터 체력 : " << hp << endl;
+	//		
+	//	}
 
-		if (hp == 0)
-		{
-			cout << "몬스터 처치!" << endl;
-			break;
-		}
+	//	if (hp == 0)
+	//	{
+	//		cout << "몬스터 처치!" << endl;
+	//		break;
+	//	}
 
-		if (round == 5)
-		{
-			cout << "제한 라운드 종료" << endl;
-			break;
-		}
-		round++;
-	}
+	//	if (round == 5)
+	//	{
+	//		cout << "제한 라운드 종료" << endl;
+	//		break;
+	//	}
+	//	round++;
+	//}
 #pragma endregion
 
 	// 가위 바위 보
+#pragma region 가위 바위 보
+	srand(time(0));
+
+	while (true)
+	{
+		cout << "가위(0), 바위(1), 보(2) 를 골라주세요" << endl;
+		cout << "> ";
+
+		int value;
+		cin >> value;
+
+		int computerValue = rand() % 3;
+
+		if (value == 0)
+		{
+			switch (computerValue)
+			{
+			 case 0:
+				 cout << value << computerValue << "비겼습니다" << endl;
+				break;
+			 case 1:
+				 cout << value << computerValue << "졌습니다" << endl;
+				 break;
+			 case 2: 
+				 cout << value << computerValue << "이겼습니다" << endl;
+				 break;
+			 default:
+				 break;
+			}
+		}
+		else if (value == 1)
+		{
+			switch (computerValue)
+			{
+			case 0:
+				cout << value << computerValue << "이겼습니다" << endl;
+				break;
+			case 1:
+				cout << value << computerValue << "비겼습니다" << endl;
+				break;
+			case 2:
+				cout << value << computerValue << "졌습니다" << endl;
+				break;
+			default:
+				break;
+			}
+		}
+		else if (value == 2)
+		{
+			switch (computerValue)
+			{
+			case 0:
+				cout << value << computerValue << "졌습니다" << endl;
+				break;
+			case 1:
+				cout << value << computerValue << "이겼습니다" << endl;
+				break;
+			case 2:
+				cout << value << computerValue << "비겼습니다" << endl;
+				break;
+			default:
+				break;
+			}
+		}
+		else
+		{
+
+		}
+	}
+#pragma endregion
+
 }
