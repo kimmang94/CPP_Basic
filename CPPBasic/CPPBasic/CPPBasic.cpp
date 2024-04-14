81915a3055e22e71fe6e73e97d6611bf2484ec59
Kimhyeok <kimmang940715@naver.com>
2024-04-14 23:32:21 +0900

    feat : Text RPG, 로비입장, 직업생성 함수 기능구현

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -1,49 +1,86 @@
 ﻿#include <iostream>
 using namespace std;
 
+// Text RPG
 
-/*
-	Stack Memory
-	      함수들이 사용하는 공용으로 사용하는 메모장
-	High
-		stack 영역은 고정되지않는다. 함수의 크기에따라 변동 (상대주소로 계산)
-		global 변수(함수) 는 따로 제거하지않으면 계속 유지된다.
-		local 변수(함수) 는 사용하고 제거되면서 공간이 정리된다.
-
-		구글 스택프레임 검색
-
-		-------------------------
-		Main()
-		
-		매개변수 (10)
-		반환 주소값 
-		-------------------------
-		Test(10);
-
-		-------------------------
-		int a = 10;
-
-
-	Low
-*/
-
-/*
-	32bit 64bit 차이점
-	- 레지스터의 크기 
-	- CPU에는 레지스터가 존재한다
-*/
-
-
-int Test(int a)
+enum PlayerType
 {
-	int c = a ;
+	PT_None = 0,
+	PT_Knight = 1,
+	PT_Archer = 2,
+	PT_Mage = 3,
+};
 
-	return c;
-}
+PlayerType playerType;
+int hp;
+int attack;
+int defence;
+
+void EnterLobby();
+void SelectPlayer();
 
 int main()
 {
-	Test(10);
+	EnterLobby();
+}
 
-	int a = 10;
+void EnterLobby()
+{
+	while (true)
+	{
+		cout << "-------------------------" << endl;
+		cout << "로비에 입장했습니다!" << endl;
+		cout << "-------------------------" << endl;
+
+		// 플레이어 직업 선택
+		SelectPlayer();
+		
+
+
+	}
+}
+
+void SelectPlayer()
+{
+	while (true)
+	{
+		cout << "-------------------------" << endl;
+		cout << "직업을 골라주세요!!" << endl;
+		cout << "[1]기사 [2] 궁수 [3] 마법사" << endl;
+		cout << "-------------------------" << endl;
+		cout << "> ";
+
+		int choice;
+		cin >> choice;
+
+		if (choice == PT_Knight)
+		{
+			cout << "기사 생성중..." << endl;
+			hp = 150;
+			attack = 10;
+			defence = 5;
+			playerType = PT_Knight;
+			break;
+		}
+		else if (choice == PT_Archer)
+		{
+			cout << "궁수 생성중..." << endl;
+			hp = 100;
+			attack = 15;
+			defence = 3;
+			playerType = PT_Archer;
+			break;
+		}
+		else if (choice == PT_Mage)
+		{
+			cout << "마법사 생성중..." << endl;
+			hp = 80;
+			attack = 25;
+			defence = 0;
+			playerType = PT_Mage;
+			break;
+		}
+
+	}
+	
 }
