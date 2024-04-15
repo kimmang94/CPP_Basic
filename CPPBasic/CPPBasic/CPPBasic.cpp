5828b397f9cc4466718695921dd34b22529cf53f
kimhyeok <kimmang940715@naver.com>
2024-04-15 17:35:21 +0900

    feat : 커서 온오프와 위치 세팅을 위한 함수 기능 구현

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -1,35 +1,27 @@
 ﻿#include <iostream>
 #include "Helper.h"
+#include <Windows.h>
+
 using namespace std;
 
-// 배열에 대한 이해
-
-struct StatInfo
+void SetCursorPosition(int x, int y)
 {
-	int hp;
-	int damage;
-	int defence;
-};
+	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos = { (SHORT)x, (SHORT)y };
+	::SetConsoleCursorPosition(output, pos);
+}
 
-// [][][][][][]
+void SetCursorOnOff(bool visible)
+{
+	HANDLE output = ::GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO cursorInfo;
+	::GetConsoleCursorInfo(output, &cursorInfo);
+	::SetConsoleCursorInfo(output, &cursorInfo);
+}
+
 int main()
 {
-	//StatInfo monster[10];
-
-	int arr[5];
-
-	int arr1[5] = { 1, 2, 3, 4, 5 };
-
-	/*arr[0] = 10;
-	arr[1] = 20;
-	arr[2] = 30;
-	arr[3] = 40;*/
-
-	for (int i = 0; i < 5; i++)
-		arr[i] = i * 10;
-
-	for (int i = 0; i < 5; i++)
-		cout << arr[i] << endl;
+	SetCursorOnOff(false);
 }
 
 
