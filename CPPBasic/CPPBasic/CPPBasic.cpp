5ac4a397c64629490ea6f337df91485ed56c20ea
kimhyeok <kimmang940715@naver.com>
2024-04-15 18:16:22 +0900

    feat : 맵생성 기능구현

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -1,11 +1,23 @@
 ﻿#include <iostream>
 #include "Helper.h"
-
+#include "Map.h"
 using namespace std;
+
 
 int main()
 {
 	SetCursorOnOff(false);
+
+	while (true)
+	{
+		// 입력
+
+		// 로직
+
+		// 출력
+
+		PrintMap1D();
+	}
 }
 
 
---
+++ CPPBasic/CPPBasic/Map.h
@@ -0,0 +1,5 @@
+#pragma once
+const int MAP_SIZE = 5;
+extern int GMap1D[MAP_SIZE * MAP_SIZE];
+
+void PrintMap1D();
