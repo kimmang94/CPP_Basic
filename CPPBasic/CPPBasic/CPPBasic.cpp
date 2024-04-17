e268cbcdec01df8a2648a53886c57c234da20d2b
kimhyeok <kimmang940715@naver.com>
2024-04-17 21:36:58 +0900

    feat : 포인터 기초 정리

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -2,15 +2,24 @@
 #include "Helper.h"
 using namespace std;
 
+void AddHP(int hp, int value)
+{
+
+}
+
 int main()
 {
-<<<<<<< HEAD
+	int hp = 100;
 
-=======
-	Test2();
+	// [타입]* [이름];
+	// - 주소값을 담는 바구니
+	// - 주소값을 타고가면 -> 무엇이 있느냐?
+	
+	int* ptr = &hp;
 
-	GTest = 10;
->>>>>>> feature/DivideFile
+	// hp주소에있는 값 호출
+	cout << *ptr << endl << &hp << endl;
+
 }
 
 
