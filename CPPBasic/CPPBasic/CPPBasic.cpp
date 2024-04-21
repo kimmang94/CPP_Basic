4202025ceab6e8dbe20e09e76001127fa899998f
kimhyeok <kimmang940715@naver.com>
2024-04-22 02:01:29 +0900

    feat : 포인터 스왑

--- CPPBasic/CPPBasic/CPPBasic.cpp
+++ CPPBasic/CPPBasic/CPPBasic.cpp
@@ -1,25 +1,25 @@
 ﻿#include <iostream>
-#include "Helper.h"
 using namespace std;
 
-void AddHP(int hp, int value)
+void Swap(int* a, int* b)
 {
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+
 
 }
 
 int main()
 {
-	int hp = 100;
 
-	// [타입]* [이름];
-	// - 주소값을 담는 바구니
-	// - 주소값을 타고가면 -> 무엇이 있느냐?
-	
-	int* ptr = &hp;
 
-	// hp주소에있는 값 호출
-	cout << *ptr << endl << &hp << endl;
+	int a = 10;
+	int b = 20;
+	Swap(&a, &b);
 
+	cout << a << endl;
+	cout << b << endl;
 }
 
 
